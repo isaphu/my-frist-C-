3d0658e801682ea5412a1b19690b833a08827c27
Isa Phuyuthanon <isaphuyuthanon@outlook.com>
2021-02-17 16:29:02 +0700

    array and accessing array

--- myfirstCsharp/Program.cs
+++ myfirstCsharp/Program.cs
@@ -18,6 +18,28 @@ namespace myfirstCsharp
             Age age = new Age();
             var dob = age.CalculateForBOD(2021, 1994);
             Console.WriteLine(dob);
+
+            var numbers = new int[5];
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+            Console.WriteLine(numbers[3]);
+            Console.WriteLine(numbers[4]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Isa", "Jack", "John" };
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+
         }
     }
 
