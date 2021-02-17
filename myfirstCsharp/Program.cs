45c3de8c59ea304ebec7488a87fb559424e5a299
Isa Phuyuthanon <isaphuyuthanon@outlook.com>
2021-02-17 15:29:28 +0700

    classes

--- myfirstCsharp/Program.cs
+++ myfirstCsharp/Program.cs
@@ -2,17 +2,28 @@
 
 namespace myfirstCsharp
 {
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + FirstName + " " + LastName);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var a = 1;
-            var b = 2;
-            var c = 3;
-
-            Console.WriteLine((a + b) * c);
+            var isa = new Person();
+            isa.FirstName = "Isa";
+            isa.LastName = "Phuyuthanon";
+            isa.Introduce();
 
         }
     }
+
 }
+
 
