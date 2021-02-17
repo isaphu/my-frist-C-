ddbfe1af3a5f94801c8e560251ff8ca242fd8a1a
Isa Phuyuthanon <isaphuyuthanon@outlook.com>
2021-02-17 15:33:04 +0700

    adding person class

--- myfirstCsharp/Program.cs
+++ myfirstCsharp/Program.cs
@@ -1,17 +1,5 @@
-﻿using System;
-
-namespace myfirstCsharp
+﻿namespace myfirstCsharp
 {
-    public class Person
-    {
-        public string FirstName;
-        public string LastName;
-
-        public void Introduce()
-        {
-            Console.WriteLine("My name is " + FirstName + " " + LastName);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
