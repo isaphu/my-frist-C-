3c284075d2e5b3f1524eeb8f9398b160f6fe3f56
Isa Phuyuthanon <isaphuyuthanon@outlook.com>
2021-02-17 17:30:50 +0700

    value type

--- myfirstCsharp/Program.cs
+++ myfirstCsharp/Program.cs
@@ -62,15 +62,19 @@ namespace myfirstCsharp
             Console.WriteLine(method.ToString()); //use method toString to convert enum to str
 
             var methodName = "Express"; //converting str to num by parsing 
-            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName)
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
 
 
+            var a = 10;
+            var b = a;
+            b++;
 
+            Console.WriteLine(string.Format("a: {0}, b: {1}", a,b));
 
-
-
-
-
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            Console.WriteLine(string.Format("array1: {0}, array2: {1}", array1[0], array2[0]));
         }
     }
 }
