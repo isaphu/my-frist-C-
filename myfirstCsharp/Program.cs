c6250b2b965800bfc23c00cfbe06177913d51b5b
Isa Phuyuthanon <isaphuyuthanon@outlook.com>
2021-02-17 14:39:40 +0700

    math operator divide and float

--- myfirstCsharp/Program.cs
+++ myfirstCsharp/Program.cs
@@ -6,17 +6,11 @@ namespace myfirstCsharp
     {
         static void Main(string[] args)
         {
-            try
-            {
-                string str = "true";
-                bool b = Convert.ToBoolean(str);
-                Console.WriteLine(b);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("The number could not convert to a byte");
+            var a = 10;
+            var b = 3;
 
-            }
+            Console.WriteLine((float)a / (float)b);
+
         }
     }
 }
