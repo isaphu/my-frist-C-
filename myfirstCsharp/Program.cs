7a933ba50e2307056bce16c97a1d4f8f336a914d
Isa Phuyuthanon <isaphuyuthanon@outlook.com>
2021-02-18 13:02:58 +0700

    adding conditional statement

--- myfirstCsharp/Program.cs
+++ myfirstCsharp/Program.cs
@@ -75,6 +75,37 @@ namespace myfirstCsharp
             var array2 = array1;
             array2[0] = 0;
             Console.WriteLine(string.Format("array1: {0}, array2: {1}", array1[0], array2[0]));
+
+
+            //conditional statments 
+            //if-else
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning");
+            }
+            else if ( hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's afternoon");
+            }
+            else
+            {
+                Console.WriteLine("It's evening");
+            }
+
+            bool isGoldCustomer = true;
+
+            /*
+            float price;
+            if (isGoldCustomer)
+                price = 19.95f;
+            else
+                price = 29.95f;
+            */
+
+            float price = (isGoldCustomer) ? 19.95f : 29.95f;
+            Console.WriteLine("You are a gold customer");
         }
     }
 }
