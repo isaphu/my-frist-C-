323aeb0ecada0576afe0f4f1146bc08563da3742
Isa Phuyuthanon <isaphuyuthanon@outlook.com>
2021-02-18 14:07:10 +0700

    adding more statement

--- myfirstCsharp/Program.cs
+++ myfirstCsharp/Program.cs
@@ -106,6 +106,52 @@ namespace myfirstCsharp
 
             float price = (isGoldCustomer) ? 19.95f : 29.95f;
             Console.WriteLine("You are a gold customer");
+
+            //iteration statment
+            
+            /*
+            for (var i = 0; i <= 10 ; i++)
+            {
+                if ( i %2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            */
+
+
+            var name = "Isa Phuyuthanon";
+
+            for (var i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            //will have the same output
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+            var i = 0;
+            while (i <= 10)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+
+                i++;
+            }
+
+            while (true)
+            {
+                Console.Write("Type your name");
+                var input = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input))
+                    break;
+                Console.WriteLine("@Echo: " + input);
+
+            }
         }
     }
 }
